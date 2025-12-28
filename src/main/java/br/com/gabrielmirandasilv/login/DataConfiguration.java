c9b96f2867b2994c89b981531d5daf0dbf0b2c3b
gabrielmirandasilv <gabrielmiranda61637@gmail.com>
2025-12-28 01:29:25 -0300

    ConnectMariaDB

---
+++ src/main/java/br/com/gabrielmirandasilv/login/DataConfiguration.java
@@ -0,0 +1,38 @@
+package br.com.gabrielmirandasilv.login;
+
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.JDBCType;
+
+import javax.sql.DataSource;
+
+import org.hibernate.Hibernate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DataConfiguration {
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.mariadb.jdbc.Driver");
+        dataSource.setUrl("jdbc:mariadb://localhost:3306/login_db?useTimezone=true&serverTimezone=UTC");
+        dataSource.setUsername("gabriel");
+        dataSource.setPassword("1907");
+        return dataSource;
+    }
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MariaDBDialect");
+        adapter.setPrepareConnection(true);
+        return adapter;
+    }
+}
